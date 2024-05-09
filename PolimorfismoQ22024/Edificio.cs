3bf378d61c8c148dc36b25439b07c8207026d316
gurdian0614 <ivan.gurdian@outlook.com>
2024-05-08 18:50:51 -0600

    Polimorfismo mediante listas

---
+++ PolimorfismoQ22024/Edificio.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolimorfismoQ22024
+{
+    public class Edificio
+    {
+        public string Tipo {  get; set; }
+
+        public double Precio { get; set; }
+
+        /// <summary>
+        /// Método que muestra el mensaje de venta
+        /// </summary>
+        public void Vender()
+        {
+            Console.WriteLine($"{Tipo} vendida por el precio de L{Precio}");
+        }
+    }
+}
--- PolimorfismoQ22024/Program.cs
+++ PolimorfismoQ22024/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using PolimorfismoQ22024;
+
+List<Edificio> edificioLista = new List<Edificio>();
+string opcion = "";
+do
+{
+    Edificio edificio = new Edificio();
+    Console.WriteLine("Ingrese tipo de edificio:");
+    edificio.Tipo = Console.ReadLine();
+    Console.WriteLine("Ingrese precio:");
+    edificio.Precio = double.Parse(Console.ReadLine());
+    edificioLista.Add(edificio);
+    Console.WriteLine("Desea continuar? (S/N)");
+    opcion = Console.ReadLine();
+} while (opcion != "N");
+
+Console.WriteLine("\n\n********** Lista de Edificios **********");
+foreach (Edificio e in edificioLista)
+{
+    e.Vender();
+}
+Console.WriteLine("********** Fin Lista de Edificios **********");
